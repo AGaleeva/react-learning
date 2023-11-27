32737f2106a3563e397189d3b20d843bdc00f10c
AGaleea <a_galeeva@hotmail.com>
2023-11-27 16:18:51 +0100

    homework17 added

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App_Homework17"; // import App, {animal} from "./App_fromLess"; // import {App} from "./App_fromLess";
+import App from "./App_homework17"; // import App, {animal} from "./App_fromLess"; // import {App} from "./App_fromLess";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
