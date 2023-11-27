cff399600e77261a9dcddeee2f8184fd71b214f1
AGaleea <a_galeeva@hotmail.com>
2023-11-27 16:15:33 +0100

    homework17 added

--- src/App_homework17.jsx
+++ src/App_homework17.jsx
@@ -1,37 +1,57 @@
 import "./app.css";
 import Button from "./components/Button";
-import Modal from "./components/Modal";
 import ProfileCard from "./components/ProfileCard";
-import Avatar from "./components/Avatar";
 
-export const animal = "Dog";
+function App() {  
+  
+  const profileInfo1 = {
+    avatar: "https://99px.ru/sstorage/1/2016/08/image_11408160131324475937.gif",
+    firstName: 'Marie',
+    lastName: 'Grinde', 
+    job: 'pet handler',
+    hairColor: 'blond',
+    height: 167,
+    hobby: 'chating',
+    homePets: {
+      dog: "Spyke",
+      cat: "Tom",
+    },
+  }
 
-function App() {
-  let name = <p>FirstName1 SecondName1</p>;
-  let job = <p>job 1</p>;  
-  const characters = {
-    hair: "black",
-    height: "185",
-  };
-  let hobby = <p>hobby 1</p>;
-   
-  console.log(buttons);
+  const profileInfo2 = {
+    avatar: "https://99px.ru/sstorage/1/2022/02/image_11602221441554073687.gif",
+    firstName: 'John',
+    lastName: 'Smith', 
+    job: 'lawyer', 
+    hairColor: 'brunette',
+    height: 183,
+    hobby: 'travelling',
+  }
+
+  const profileInfo3 = {
+    avatar: "https://99px.ru/sstorage/1/2020/09/image_10409200805469676138.jpg",
+    firstName: 'Ewan',
+    lastName: 'Bob', 
+    job: 'sales manager',
+    hairColor: 'brown',
+    height: 179,
+    hobby: 'meditate',
+  }
 
   return (
     <div className="App">          
     
       <div className="profileCard-container">
-        <ProfileCard>           
-          <p></p>
-          <p character = "brown"></p>
-          <p character = {characters.height}></p>
-          <p></p>          
-        </ProfileCard>
-
+        <ProfileCard profileInfo={profileInfo1} />
       </div>        
+      <div className="profileCard-container">
+        <ProfileCard profileInfo={profileInfo2} />        
+      </div>        
+      <div className="profileCard-container">
+        <ProfileCard profileInfo={profileInfo3} />        
+      </div>           
       
     </div>   
   );  
 }
 export default App;
-// export {App};
--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -2,21 +2,33 @@ import "./styles.css"
 
 function ProfileCard({profileInfo}) { 
   return <div className="profileCard-component"> 
-  <img src= {profileInfo.avatar} alt= {`${profileInfo.firstName} ${profileInfo.secondName}`} />   
-    <h2>
-      {profileInfo.firstName}
-      {profileInfo.secondName}
-    </h2>   
+  <img className="avatar" src= {profileInfo.avatar} alt= {`${profileInfo.firstName} ${profileInfo.lastName}`} />   
+    <h3>
+      {`${profileInfo.firstName} ${profileInfo.lastName}`}
+    </h3>   
     <p>
-      {job}
+      {`Occupation: ${profileInfo.job}`}
     </p>
     <ul>
-      <li>{profileInfo.hairColor}</li>
-      <li>{profileInfo.height}</li>
+      <li>{`Hair color: ${profileInfo.hairColor}`}</li>
+      <li>{`Height: ${profileInfo.height}`}</li>      
     </ul>
     <p>
-      {profileInfo.hobby}
-    </p>    
+      {`Hobby: ${profileInfo.hobby}`}
+    </p>
+    {/* 1 из способов показывать или не показывать данные на странице через тернарный оператор */}
+      {/* {profileInfo.homePets ? <p>Home pets:</p> : null} */}
+      {/* 2 cпособ: синтаксис &&. Левая часть - это ture или false, 
+      а правая - это то что мы отображаем, если у нас левая часть = true */}
+      {profileInfo.homePets && <p>Home pets:</p>}
+      <ul>
+        {profileInfo?.homePets?.dog ? (
+          <li>Dog: {profileInfo?.homePets.dog}</li>
+        ) : null}
+        {profileInfo?.homePets?.cat ? (
+          <li>Cat: {profileInfo?.homePets.cat}</li>
+        ) : null}
+      </ul>
   </div>
 }
 
