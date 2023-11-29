756dedab8daff7b72d53191bf4681f12b098a930
AGaleea <a_galeeva@hotmail.com>
2023-11-29 11:15:26 +0100

    changes on lesson

--- src/homeworks/homework19/Homework19.jsx
+++ src/homeworks/homework19/Homework19.jsx
@@ -2,15 +2,24 @@ import { useState } from 'react';
 import Button from '../../components/Button';
 import './styles.css'
 
+// 
+const porscheCar = { 
+  brand: "Porsche", 
+  model: "911", 
+  year: 2023, 
+  color: "Silver", 
+  price: 120000, 
+  isAvailable: true,
+};
+
 function Homework19() {
 
   const [car, setCar] = useState({});
-
+  
   const showCar = (newCar) => {
-    setCar(newCar); 
+      setCar(newCar); 
   };
 
-  
   //  const showCar = (newCar) => {
   //   setCar(prevValue => (newCar)); 
 
@@ -18,35 +27,52 @@ function Homework19() {
   // например: setCar(prevValue => ({...prevValue, ...newCar}));
 
 
+  // создаем динамически массив эл-в <li></li> с использованием метода Object.keys(), получая массив ключей ['brand', 'model', 'year', 'color', 'price', 'isAvailable']. carKey - каждый из ключей массива, по которому мы достаем значение на каждой итерации цикла метода map - car[carKey].
 
-  const currCar = Object.keys(car).map((carKeys) => {
+  // первичный метод
+
+  // const currCar = Object.keys(car).map((carKey) => {
+  //   return (
+  //     <li key={`car-props-${Math.random()}`}>
+  //       {carKey}: {`${car[carKey]}`}
+  //     </li>
+  //   )
+  // })
+
+  // модернизация метода
+
+  const currCar = Object.keys(car).map((carKey) => {
+
+    const carContent = () => {
+    if (carKey === 'isAvailable') {
+      if (car[carKey]) {
+        return 'Available';
+      }
+      return 'Unavailable';
+    }
+    return `${carKey}: ${car[carKey]}`
+  }
     return (
       <li key={`car-props-${Math.random()}`}>
-        {carKeys}: {car[carKeys]}
+        {carContent()}
       </li>
     )
   })
 
   return (
     <div className="homework19-wrapper">
-
-      <div className='itemsControl'>
-        Car's properties: 
-        <ul>{currCar}</ul>
-      </div>
-
+      
       <div className='buttonControl'>
         <Button 
         name='show properties of the car' 
-        onClick={() => showCar({
-          brand: "Porsche", 
-          model: "911", 
-          year: 2023, 
-          color: "Silver", 
-          price: 120000, 
-          isAvailable: true,})} 
+        onClick={() => showCar(porscheCar)} 
         />
       </div> 
+
+      <div className='itemsControl'>
+        Car's properties: 
+        {!!Object.keys(car).length && <ul>{currCar}</ul>} // !! проверяет выражение - true или false
+      </div>
     </div>
   );
 };
