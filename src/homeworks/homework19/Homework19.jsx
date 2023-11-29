d3129ffc690ad18359e514c6636d7bb9a82177aa
AGaleea <a_galeeva@hotmail.com>
2023-11-29 09:35:29 +0100

    fixed

--- src/homeworks/homework19/Homework19.jsx
+++ src/homeworks/homework19/Homework19.jsx
@@ -7,8 +7,17 @@ function Homework19() {
   const [car, setCar] = useState({});
 
   const showCar = (newCar) => {
-    setCar(prevValue => (newCar)); 
+    setCar(newCar); 
   };
+
+  
+  //  const showCar = (newCar) => {
+  //   setCar(prevValue => (newCar)); 
+
+  // в данном случае вы не используете prevValue - соответсвенно функция обратного вызова вам не нужна если вы просто хотите передать туда новый обьект и вам не нужно знать что там было до него. Если же вам нужен предыдущий обьект в car, тогда нужно например обьеденить с новым как мы делали в классе:
+  // например: setCar(prevValue => ({...prevValue, ...newCar}));
+
+
 
   const currCar = Object.keys(car).map((carKeys) => {
     return (
@@ -37,7 +46,7 @@ function Homework19() {
           price: 120000, 
           isAvailable: true,})} 
         />
-      </div> //
+      </div> 
     </div>
   );
 };
